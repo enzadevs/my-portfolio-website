b625f14e69e6f8ab9b43077c946b76c8e22e926c
Enza <79910926+enzadevs@users.noreply.github.com>
2023-10-31 17:44:11 +0500

    Removed junk code

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,26 +1,18 @@
 export const metadata = {
-    title: 'NextJS',
-    description: 'App',
+    title: 'Enza Devs',
+    description: 'Young and ambitious web developer from Turkmenistan.',
     name: 'viewport', 
     content:'width=device-width, initial-scale=1'
 }
 
-import 'styles/global.css'
-import { StrictMode } from 'react'
-import localFont from 'next/font/local'
+import './global.css'
 
-const InterFont = localFont({
-    src: '../public/fonts/inter-regular.ttf'
-})
-
-export default function RootLayout({ children }) {
+export default function RootLayout({children}){
     return (
-        <StrictMode>
-            <html lang='en' className={InterFont.className}>
-                <body>
-                    {children}
-                </body>
-            </html>
-        </StrictMode>
-  )
+        <html lang='en'>
+            <body>
+                {children}
+            </body>
+        </html>
+    )
 }
